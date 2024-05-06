e4f49d16e1b257836bee5dabfcfa3718051bfd57
nhandzai <ducnhan1522004@gmail.com>
2024-05-07 00:09:15 +0700

    can play smothly

--- crew.py
+++ crew.py
@@ -1,17 +1,17 @@
 import random
 
-sus = "SUS"
-notSus = "not SUS"
+
 scripts = [
     "Above me is ",
     "Below me is ",
     "To my left is ",
     "To my right is ",
     "Next to me is ",
-    "The top row has at least ",
-    "The bottom row has at least ",
-    "The left-most column has at least ",
-    "The right-most column has at least ",
+    "The top row has ",
+    "The bottom row has ",
+    "The left-most column has ",
+    "The right-most column has ",
+
 ]
 
 coordinates = [
@@ -20,40 +20,76 @@ coordinates = [
 
 
 class Crew:
-    def __init__(self, name, pos, n, m, isLie=False) -> None:
+    def __init__(self, name, pos, n, m, color="red", is_lie=False) -> None:
         self.name = name
         self.pos = pos
         self.n = n
         self.m = m
-        self.isLie = isLie
+        self.is_lie = is_lie
+        self.color = color
+        self.name += color
 
-    def checker(self, map_data):
+    def checker(self, manager):
         if self.randomScriptIndex in [0, 1, 2, 3]:
             x, y = self.pos[0] + coordinates[self.randomScriptIndex][0], self.pos[1] + \
                 coordinates[self.randomScriptIndex][1]
             if 0 <= x < self.n and 0 <= y < self.m:
-                if map_data[x][y].isLie:
-                    self.script += sus
+                if manager.map_data[x][y].is_lie:
+                    self.script = scripts[self.randomScriptIndex] + "a liar"
                 else:
-                    self.script += notSus
+                    self.script = scripts[self.randomScriptIndex] + \
+                        "not a liar"
             else:
                 self.script += "OUT OF BOUNDS"
-        elif self.randomScriptIndex in [5, 6]:
-            row_index = 0 if self.randomScriptIndex == 5 else self.n-1
-            for j in range(self.m):
-                if map_data[row_index][j].isLie:
-                    self.script += "1 " + sus
-                    return
-            self.script += "0 " + sus
-        elif self.randomScriptIndex in [7, 8]:
-            col_index = 0 if self.randomScriptIndex == 7 else self.m-1
-            for i in range(self.n):
-                if map_data[i][col_index].isLie:
-                    self.script += "1" + sus
-                    return
-            self.script += "0 " + sus
 
-    def getScript(self, map_data):
+        elif self.randomScriptIndex == 4:
+            x_l, y_l = self.pos[0] + coordinates[2][0], self.pos[1] + \
+                coordinates[2][1]
+            x_r, y_r = self.pos[0] + coordinates[3][0], self.pos[1] + \
+                coordinates[3][1]
+            is_lie_found = 0
+            for (x, y) in [(x_l, y_l), (x_r, y_r)]:
+                if 0 <= x < self.n and 0 <= y < self.m:
+                    if manager.map_data[x][y].is_lie:
+                        is_lie_found += 1
+                        break
+            if is_lie_found:
+                self.script = scripts[self.randomScriptIndex] + "a liar"
+            else:
+                self.script = scripts[self.randomScriptIndex] + "not a liar"
+
+        elif self.randomScriptIndex in [5, 6, 7, 8]:
+            if self.randomScriptIndex in [5, 6]:
+                row_index = 0 if self.randomScriptIndex == 5 else self.n - 1
+                iterable_range = range(self.m)
+            else:
+                col_index = 0 if self.randomScriptIndex == 7 else self.m - 1
+                iterable_range = range(self.n)
+
+            is_lie_found = 0
+            for index in iterable_range:
+                if self.randomScriptIndex in [5, 6]:
+                    if manager.map_data[row_index][index].is_lie:
+                        is_lie_found += 1
+                else:
+                    if manager.map_data[index][col_index].is_lie:
+                        is_lie_found += 1
+            if self.is_lie:
+                is_lie_found = 0 if is_lie_found else 1
+
+            self.script = scripts[self.randomScriptIndex] + \
+                f'{is_lie_found} liar'
+
+        if self.is_lie:
+            if "not" in self.script:
+                self.script = self.script.replace("not ", "", 1)
+            else:
+                index = self.script.find("a liar")
+                if index != -1:
+                    self.script = self.script[:index] + \
+                        "not " + self.script[index:]
+
+    def getScript(self, manager):
         while True:
             self.randomScriptIndex = random.randint(0, len(scripts)-1)
             if self.randomScriptIndex < 4:
@@ -63,5 +99,4 @@ class Crew:
                     break
             else:
                 break
-        self.script = scripts[self.randomScriptIndex]
-        self.checker(map_data)
+        self.checker(manager)
--- manager.py
+++ manager.py
@@ -1,50 +1,62 @@
 import crew
 import random
 
+colors = [
+    "red",
+    "blue",
+    "green"
+]
+
 
 class GameManager:
-    def __init__(self, n, m, k):
+    def __init__(self, n, m, max_liar):
         self.n = n
         self.m = m
-        self.k = k
+        self.max_liar = max_liar
+        self.color_range = []
         self.map_data = self.create_map()
 
     def create_map(self):
         map_data = []
         names = [chr(65 + i) for i in range(self.n * self.m)]
         index = 0
-        lies_positions = random.sample(range(self.n * self.m), self.k)
+        lies_positions = random.sample(range(self.n * self.m), self.max_liar)
         for i in range(self.n):
             row = []
             for j in range(self.m):
+                color = random.choice(colors)
                 if index in lies_positions:
                     row.append(
-                        crew.Crew(names[index], (i, j), self.n, self.m, True))
+                        crew.Crew(names[index], (i, j), self.n, self.m, color, True))
                 else:
-                    row.append(crew.Crew(names[index], (i, j), self.n, self.m))
+                    row.append(
+                        crew.Crew(names[index], (i, j), self.n, self.m, color))
                 index += 1
+            if color != self.color_range:
+                self.color_range.append(color)
             map_data.append(row)
 
         return map_data
 
     def print_map(self):
-        max_name_length = max(len(element.name) for row in self.map_data for element in row)
-        max_script_length = max(len(element.script) if not element.isLie else len('(isLie)') for row in self.map_data for element in row)
-        
+        max_name_length = max(len(element.name)
+                              for row in self.map_data for element in row)
+        max_script_length = max(len(element.script) if not element.is_lie else len(
+            '(isLie)') for row in self.map_data for element in row)
+
         for row in self.map_data:
             for element in row:
-                if element.isLie:
-                    print(f'{element.name:<{max_name_length}} (isLie)'.ljust(max_name_length + max_script_length + 2), end='')
+                if element.is_lie:
+                    print(f'{element.name:<{max_name_length}} ({element.script}) (isLie)'.ljust(
+                        max_name_length + max_script_length + 4), end='')
                 else:
-                    print(f'{element.name:<{max_name_length}} ({element.script})'.ljust(max_name_length + max_script_length + 2), end='')
+                    print(f'{element.name:<{max_name_length}} ({element.script})'.ljust(
+                        max_name_length + max_script_length + 4), end='')
             print()
-
-
-
 
 
 manager = GameManager(3, 3, 1)
 for row in manager.map_data:
     for element in row:
-        element.getScript(manager.map_data)
+        element.getScript(manager)
 manager.print_map()
